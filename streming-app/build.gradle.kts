5a51dddf5b020f3e7e53f0548a57f3d1c901a4bb
Dinis Cunha <dinis@dcunha.pt>
2025-10-01 22:00:27 +0100

    add libraries streaming app

--- streming-app/build.gradle.kts
+++ streming-app/build.gradle.kts
@@ -31,13 +31,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
 }
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+    }
+}
+
 
 dependencies {
 
