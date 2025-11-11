4579a531ced21fb6f791435231b8ad567ae8300a
Dinis Cunha <dinis@dcunha.pt>
2025-11-11 17:03:56 +0000

    clear comments

--- streming-app/src/main/java/com/example/netflix/model/Movie.kt
+++ streming-app/src/main/java/com/example/netflix/model/Movie.kt
@@ -6,9 +6,7 @@ data class Movie(
     val name: String = "",
     val description: String = "",
     val genre: Int = 0,
-  //  val duration: String = "",
     var thumbnailPath: String = "",
     var videoPath:String = ""
- //   val videoUrl1080p: String = "",
-   // val videoUrl360p: String = ""
+
 )
